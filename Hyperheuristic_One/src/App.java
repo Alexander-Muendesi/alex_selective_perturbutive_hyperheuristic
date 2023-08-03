e881e1e80336cdf9a9602177d7d7b9814ec3ed5f
Alexander-Muendesi <u20426918@tuks.co.za>
2023-08-03 12:27:35 +0200

    🐛 Fixed some small errors in the datasets from formatting

--- Hyperheuristic_One/src/App.java
+++ Hyperheuristic_One/src/App.java
@@ -1,25 +1,19 @@
 import data_classes.Data;
 import data_classes.DataReader;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        DataReader r = new DataReader(18);
-        Data[] data = r.getData();
+        // DataReader r = new DataReader(18);
+        // Data[] data = r.getData();
 
-        for(Data d: data){
-            d.print();
-        }
-
-        // for(int i=1;i<22;i++){
-        //     System.out.println(i);
-        //     DataReader r = new DataReader(i);
+        // for(Data d: data){
+        //     d.print();
         // }
+
+        for(int i=1;i<22;i++){
+            System.out.println(i);
+            DataReader r = new DataReader(i);
+        }
     }
 }
 
